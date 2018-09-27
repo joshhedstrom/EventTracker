fea63c6201a22559704c95949ac82b3f8c6f49e1
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-09-27 12:49:33 -0600

    added events list json

---
+++ src/main/java/com/pluralsight/controller/EventsReportController.java
@@ -0,0 +1,35 @@
+package com.pluralsight.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pluralsight.model.Event;
+
+@RestController
+public class EventsReportController {
+
+	@RequestMapping("/events")
+	public List<Event> getEvents() {
+
+		List<Event> events = new ArrayList<>();
+
+		Event event1 = new Event();
+		event1.setName("Java User Group");
+		events.add(event1);
+
+		Event event2 = new Event();
+		event2.setName("Angular User Group");
+		events.add(event2);
+
+		Event event3 = new Event();
+		event3.setName("Spring User Group");
+		events.add(event3);
+
+		return events;
+
+	}
+
+}
